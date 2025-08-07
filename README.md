ddd6ede1f1e7afca38526b913b19a7336d4ea31d
geloxh <ace.blazado7@gmail.com>
2025-08-07 21:28:19 +0800

    First Commit for Scientific Calculator In Java

---
+++ README.md
@@ -0,0 +1,10 @@
+### Scientific Calculator In Java
+
+## Steps To Create Scientific Calculator In Java
+
+- Importing Packages and Main Class Setup
+- Constructor to Main Class
+- Implementing ActionListener Interface
+- Evaluating the Expression
+- The main () method
+- Test the Scientific Calculator
---
+++ ScientificCalculator.java
@@ -0,0 +1,24 @@
+// Import Packages And Main Class Setup
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+
+public class ScientificCalculator extends Jframe implements ActionListener;
+
+public ScientificCalculator() {
+    setTitle("Scientific Calculator In Java");
+    setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    setLayout(new BorderLayout());
+
+    textField = new JTextField();
+    textField.setEditable(false);
+    textField.setPreferredSize(new Dimension(300, 40));
+    add(textField, BorderLayout.NORTH);
+
+    JPanel buttonPanel = new JPanel();
+    buttonPanel.setLayout(new GridLayout(9, 4));
+    buttonPanel.setBackground(Color.ORANGE);
+    buttonPanel.setForeground(Color.PINK);
+
+    // Button Labels For The Calculator
+}
